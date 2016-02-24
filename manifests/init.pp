3e5d8aa379487778d16b84257055f7cc025b2075
Paul Belanger <pabelanger@redhat.com>
2016-02-24 16:26:54 -0500

    Allow user to pass a hash of captcha questions
    Change-Id: I102ff6991831901c578dab4107b0c23245da9d4b
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,7 +14,9 @@ class mediawiki(
   $wg_dbpassword = undef,
   $wg_secretkey = undef,
   $wg_upgradekey = undef,
-  $wg_captchaquestions_answer = undef,
+  # TODO(pabelanger): Remove this once 284418 lands.
+  $wg_captchaquestions_answer = undef
+  $wg_captchaquestions = {},
   $wg_googleanalyticsaccount = undef,
 ) {
 
