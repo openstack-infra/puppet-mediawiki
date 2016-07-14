80feab8c4de3f21072e08f9cdc78637fdf5eba56
Jeremy Stanley <fungi@yuggoth.org>
2016-07-14 19:37:58 +0000

    Clean up unused questy captcha variable
    Once the dependency for this merges to system-config, remove the
    unused wg_captchaquestions variable.
    
    Change-Id: If3ac86edc27bda317fe3a29f984dc9a2acd79e31
    Depends-On: I0f184c9bfa2bdaaca2478500d8f3b418c9bf77ae

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,8 +13,6 @@ class mediawiki(
   $ssl_key_file_contents      = undef,  # If left empty puppet will not create file.
   $wg_recaptchapublickey      = undef,
   $wg_recaptchaprivatekey     = undef,
-  # TODO(fungi): wg_captchaquestions can be removed once 342372 merges
-  $wg_captchaquestions        = {},
   $wg_googleanalyticsaccount  = undef,
   $wg_dbpassword              = undef,
   $wg_secretkey               = undef,
