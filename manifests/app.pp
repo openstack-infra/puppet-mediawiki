e6edf5277a78837ccbdbc438142bb4de041a8202
Alex Monk <krenair@gmail.com>
2016-08-06 19:32:05 +0000

    Ensure latest on Trusty
    Change-Id: I418eb2bd16538517be0829c50bca1e8dad60d894

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,18 +1,22 @@
 # Class: mediawiki::app
 #
 class mediawiki::app {
+  $ensure = $::lsbdistcodename ? {
+    'precise' => present,
+    'trusty'  => latest, # keep up to date
+  }
   $revision = $::lsbdistcodename ? {
     'precise' => 'origin/master', # madness
     'trusty'  => 'origin/REL1_27',
   }
   vcsrepo { '/srv/mediawiki/w':
-    ensure   => present,
+    ensure   => $ensure,
     provider => git,
     source   => 'https://gerrit.wikimedia.org/r/p/mediawiki/core.git',
     revision => $revision,
   }
   vcsrepo { '/srv/mediawiki/w/vendor':
-    ensure   => present,
+    ensure   => $ensure,
     provider => git,
     source   => 'https://gerrit.wikimedia.org/r/p/mediawiki/vendor.git',
     revision => $revision,
