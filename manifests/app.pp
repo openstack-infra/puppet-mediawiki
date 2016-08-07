d390b894a325d62881210d70f9a873bebdad3cc9
Alex Monk <krenair@gmail.com>
2016-08-07 01:51:43 +0000

    Ensure cache directory is owned by the right user/group
    Change-Id: I72ac640b70675561ef32f906b47f6d900e76a028

--- manifests/app.pp
+++ manifests/app.pp
@@ -21,6 +21,12 @@ class mediawiki::app {
     source   => 'https://gerrit.wikimedia.org/r/p/mediawiki/vendor.git',
     revision => $revision,
   }
+  file { '/srv/mediawiki/w/cache':
+    ensure  => directory,
+    recurse => true,
+    owner   => 'www-data',
+    group   => 'www-data',
+  }
 }
 
 # vim:sw=2:ts=2:expandtab:textwidth=79
