4cbe39375c977bbdc583bc96456a61dae5eb9784
Jeremy Stanley <fungi@yuggoth.org>
2016-08-19 20:36:11 +0000

    Standardize HTTPS and vhost configuration
    Adjust the SSL/TLS X.509 file handling to match our flexible
    standard, which allows the deployer to choose between managing the
    contents in or outside Puppet while still providing for a sane
    snakeoil fallback to ease testing and development. Also tune the SSL
    configuration to our typical stricter defaults, unconditionally
    redirect all HTTP requests to HTTPS, and reformat the vhost
    configuration for consistency and ease of future maintenance.
    
    Change-Id: Id5241377665845f8c453cbb833bc40d3a5915d76

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,12 +5,13 @@ class mediawiki(
   $mediawiki_images_location  = '/srv/mediawiki/images',
   $role                       = 'all',
   $site_hostname              = $::fqdn,
-  $ssl_cert_file              = '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-  $ssl_cert_file_contents     = undef, # If left empty puppet will not create file.
+  $serveradmin                = "webmaster@${::fqdn}",
+  $ssl_cert_file              = undef,
+  $ssl_cert_file_contents     = undef,
   $ssl_chain_file             = undef,
-  $ssl_chain_file_contents    = undef, # If left empty puppet will not create file.
-  $ssl_key_file               = '/etc/ssl/private/ssl-cert-snakeoil.key',
-  $ssl_key_file_contents      = undef,  # If left empty puppet will not create file.
+  $ssl_chain_file_contents    = undef,
+  $ssl_key_file               = undef,
+  $ssl_key_file_contents      = undef,
   $wg_recaptchapublickey      = undef,
   $wg_recaptchaprivatekey     = undef,
   $wg_googleanalyticsaccount  = undef,
@@ -95,42 +96,129 @@ class mediawiki(
       ensure => present,
     }
 
-    if $ssl_cert_file_contents != undef {
-      file { $ssl_cert_file:
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0640',
-        content => $ssl_cert_file_contents,
-        before  => Httpd::Vhost[$site_hostname],
+    # To use the standard ssl-certs package snakeoil certificate, leave both
+    # $ssl_cert_file and $ssl_cert_file_contents empty. To use an existing
+    # certificate, specify its path for $ssl_cert_file and leave
+    # $ssl_cert_file_contents empty. To manage the certificate with puppet,
+    # provide $ssl_cert_file_contents and optionally specify the path to use for
+    # it in $ssl_cert_file.
+    if ($ssl_cert_file == undef) and ($ssl_cert_file_contents == undef) {
+      $cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+      if ! defined(Package['ssl-cert']) {
+        package { 'ssl-cert':
+          ensure => present,
+          before => Httpd::Vhost[$site_hostname],
+        }
+      }
+    } else {
+      if $ssl_cert_file == undef {
+        $cert_file = "/etc/ssl/certs/${::fqdn}.pem"
+        if ! defined(File['/etc/ssl/certs']) {
+          file { '/etc/ssl/certs':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+            before => File[$cert_file],
+          }
+        }
+      } else {
+        $cert_file = $ssl_cert_file
+      }
+      if $ssl_cert_file_contents != undef {
+        file { $cert_file:
+          ensure  => present,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => $ssl_cert_file_contents,
+          before  => Httpd::Vhost[$site_hostname],
+        }
       }
     }
 
-    if $ssl_key_file_contents != undef {
-      file { $ssl_key_file:
-        owner   => 'root',
-        group   => 'ssl-cert',
-        mode    => '0640',
-        content => $ssl_key_file_contents,
-        before  => Httpd::Vhost[$site_hostname],
+    # To avoid using an intermediate certificate chain, leave both
+    # $ssl_chain_file and $ssl_chain_file_contents empty. To use an existing
+    # chain, specify its path for $ssl_chain_file and leave
+    # $ssl_chain_file_contents empty. To manage the chain with puppet, provide
+    # $ssl_chain_file_contents and optionally specify the path to use for it in
+    # $ssl_chain_file.
+    if ($ssl_chain_file == undef) and ($ssl_chain_file_contents == undef) {
+      $chain_file = undef
+    } else {
+      if $ssl_chain_file == undef {
+        $chain_file = "/etc/ssl/certs/${::fqdn}_intermediate.pem"
+        if ! defined(File['/etc/ssl/certs']) {
+          file { '/etc/ssl/certs':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+            before => File[$chain_file],
+          }
+        }
+      } else {
+        $chain_file = $ssl_chain_file
+      }
+      if $ssl_chain_file_contents != undef {
+        file { $chain_file:
+          ensure  => present,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => $ssl_chain_file_contents,
+          before  => Httpd::Vhost[$site_hostname],
+        }
       }
     }
 
-    if $ssl_chain_file_contents != undef {
-      file { $ssl_chain_file:
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0640',
-        content => $ssl_chain_file_contents,
-        before  => Httpd::Vhost[$site_hostname],
+    # To use the standard ssl-certs package snakeoil key, leave both
+    # $ssl_key_file and $ssl_key_file_contents empty. To use an existing key,
+    # specify its path for $ssl_key_file and leave $ssl_key_file_contents empty.
+    # To manage the key with puppet, provide $ssl_key_file_contents and
+    # optionally specify the path to use for it in $ssl_key_file.
+    if ($ssl_key_file == undef) and ($ssl_key_file_contents == undef) {
+      $key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'
+      if ! defined(Package['ssl-cert']) {
+        package { 'ssl-cert':
+          ensure => present,
+          before => Httpd::Vhost[$site_hostname],
+        }
+      }
+    } else {
+      if $ssl_key_file == undef {
+        $key_file = "/etc/ssl/private/${::fqdn}.key"
+        if ! defined(File['/etc/ssl/private']) {
+          file { '/etc/ssl/private':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0700',
+            before => File[$key_file],
+          }
+        }
+      } else {
+        $key_file = $ssl_key_file
+      }
+      if $ssl_key_file_contents != undef {
+        file { $key_file:
+          ensure  => present,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0600',
+          content => $ssl_key_file_contents,
+          before  => Httpd::Vhost[$site_hostname],
+        }
       }
     }
 
     ::httpd::vhost { $site_hostname:
-      port     => 443,
-      docroot  => 'MEANINGLESS ARGUMENT',
-      priority => '50',
-      template => 'mediawiki/apache/mediawiki.erb',
-      ssl      => true,
+      port       => 443, # Is required despite not being used.
+      docroot    => '/var/www',
+      priority   => '50',
+      template   => 'mediawiki/apache/mediawiki.erb',
+      ssl        => true,
+      vhost_name => $site_hostname,
     }
     httpd_mod { 'rewrite':
       ensure => present,
