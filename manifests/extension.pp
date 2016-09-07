9f15d0e344e2ad5cbd1d25e4ca9ab34e8761be15
Jeremy Stanley <fungi@yuggoth.org>
2016-09-07 19:00:04 +0000

    Conditionally define the default extension source
    Since $type and $source are both class parameters, we can't
    interpolate $type in the default definition for $source. Instead
    redefine $source inside the resource if it's unchanged from the
    resource default.
    
    Change-Id: I9b447173d4966f03a195825047cf25afc1a4afd6

--- manifests/extension.pp
+++ manifests/extension.pp
@@ -1,11 +1,16 @@
 define mediawiki::extension (
   $type     = 'extension',
   $ensure   = latest, # keep up to date
-  $source   = "https://gerrit.wikimedia.org/r/p/mediawiki/${type}s/${name}.git",
+  $source   = undef, # actual default conditionally applied below
   $revision = 'origin/REL1_27',
 ) {
   if $type != 'extension' and $type != 'skin' {
     fail( '$type must be extension or skin' )
+  }
+  if $source == undef {
+    # Set our actual default for $source here since we can't interpolate $type
+    # in it for the resource parameter default
+    source = "https://gerrit.wikimedia.org/r/p/mediawiki/${type}s/${name}.git"
   }
   vcsrepo { "/srv/mediawiki/w/${type}s/${name}":
     ensure   => $ensure,
